d1af60aeaba9f0cf9416460e84ea07a2eff2e6fc
Vinícius César Alves <viniciuscesar.3003@gmail.com>
2024-05-17 11:04:23 -0300

    cores

--- Parametros.py
+++ Parametros.py
@@ -11,16 +11,16 @@ time.sleep(3)
 print('Aqui estão os seus parametros para uma vida mais saudável!')
 time.sleep(1)
 if imc < 17:
-    print('Você está muito abaixo do peso! Recomenda-se procurar um profissíonal de nutrição e educador físico!')
+    print('Você está \033[31mmuito abaixo do peso!\033[m Recomenda-se procurar um profissíonal de nutrição e educador físico!')
 elif 17 <= imc <= 18.4:
-    print('Você está abaixo do peso, {}! Recomenda-se buscar um profissíonal da área de nutrição para auxilia-lo'.format(nome))
+    print('Você está \033[33mabaixo do peso\033[m, {}! Recomenda-se buscar um profissíonal da área de nutrição para auxilia-lo'.format(nome))
 elif 18.5 <= imc <= 24.9:
-    print('Você está no peso ideal, {}. Parabéns!'.format(nome))
+    print('Você está no \033[32mpeso ideal\033[m, {}. Parabéns!'.format(nome))
 elif 25 <= imc <= 29.9:
-    print('Você está acima do peso, {}! Recomenda-se procurar um nutricionista e um educador físico.')
+    print('Você está \033[33macima do peso\033[m, {}! Recomenda-se procurar um nutricionista e um educador físico.'.format(nome))
 elif 30 <= imc <= 34.9:
-    print('Cuidado, {}! Você está em obesidade grau I! Procure um nutricionsta e um educador físico!'.format(nome))
+    print('Cuidado, {}! Você está em \033[31mobesidade grau I\033[m ! Procure um nutricionsta e um educador físico!'.format(nome))
 elif 35 <= imc <= 40:
-    print('Cuidado, {}! Voce está em obesidade grau II! Procure um nutricionista e ducador físico!'.format(nome))
+    print('Cuidado, {}! Voce está em \033[31mobesidade grau II!\033[m Procure um nutricionista e ducador físico!'.format(nome))
 else:
-    print('Você está em obesidade grau III, {}! Muito cuidado! Procure um clínico geral, nutricionsta e educador físico urgente!'.format(nome))
+    print('Você está em \033[31mobesidade grau III\033[m, {}! Muito cuidado! Procure um clínico geral, nutricionsta e educador físico urgente!'.format(nome))
