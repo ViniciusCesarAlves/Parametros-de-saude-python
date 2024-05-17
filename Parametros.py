a939639efa519a1b98954522441e45bf70338327
Vinícius César Alves <viniciuscesar.3003@gmail.com>
2024-05-16 23:36:55 -0300

    Initial commit

---
+++ Parametros.py
@@ -0,0 +1,7 @@
+import time, math
+nome = (input('Olá! Qual o seu nome? '))
+n = nome.strip()
+peso = float(input('{}, qual o seu peso atual em Kg? '.format(n)))
+print('Processando os seus dados.')
+print('AGUARDE...')
+time.sleep(3)
